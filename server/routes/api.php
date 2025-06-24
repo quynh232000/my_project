635933b9a091d2801c7f46454d76c13d518f3c71
quynh <quynh232000@gmail.com>
2025-06-24 21:00:32 +0700

    server: set cors

--- server/routes/api.php
+++ server/routes/api.php
@@ -1,9 +1,10 @@
 <?php
 
 use App\Http\Middleware\ApiMiddleware;
+use App\Http\Middleware\CorsMiddleware;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware([ApiMiddleware::class])->group(function () {
+Route::middleware([ApiMiddleware::class,CorsMiddleware::class])->group(function () {
     Route::group(['prefix' => 'v1', 'as' => 'api.v1.'], function () {
         require __DIR__ . '/api/v1.php';
     });
