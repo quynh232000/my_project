b253dbac168864ee0e2db5ed125455eabf5f7090
Mr Quynh <97020966+quynh232000@users.noreply.github.com>
2025-08-01 11:42:39 +0700

    Update page.tsx

--- client/src/app/(app)/blog/[blog_slug]/page.tsx
+++ client/src/app/(app)/blog/[blog_slug]/page.tsx
@@ -3,11 +3,12 @@ import Container from "./components/Container";
 export default async function Page({
   params,
 }: {
-  params: { blog_slug: string };
+  params: Promise<{ blog_slug: string };>
 }) {
+  const {blog_slug} = await params
   return (
     <div className="mt-[148px]">
-      <Container blog_slug={params.blog_slug} />
+      <Container blog_slug={blog_slug} />
     </div>
   );
 }
