34f8286f8f3ac0134fc795046346900875f6e0f8
Mr Quynh <97020966+quynh232000@users.noreply.github.com>
2025-08-08 08:36:55 +0700

    Update page.tsx

--- client/src/app/(app)/page.tsx
+++ client/src/app/(app)/page.tsx
@@ -1,13 +1,14 @@
 'use client';
 import Container from './components/Container';
-
+import { Suspense } from 'react';
 function page() {
 
 	
 	return <div className='mt-[148px]'>
 		{/* banner search */}
-		
+		<Suspense fallback={<div>Đang tải...</div>}>
 		<Container/>
+    </Suspense>
 	</div>;
 }
 
